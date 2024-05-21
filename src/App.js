6dd3d2550f3d54c787bdd92be9b8806cdd602e0b
Touradou <cheikhtouradougueye@esp.sn>
2024-05-21 03:23:56 +0000

    header et bienvenue

--- src/App.js
+++ src/App.js
@@ -1,56 +1,14 @@
+import Bienvenue from './components/bienvenue';
+import Header from './components/header';
 import React from 'react';
 ;
 
 function App() {
     return (
         <div>
-            <header id="header">
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="#accueil" id="accueil">Accueil</a>
-                            <ul>
-                                <li><a href="#à propos">A propos</a></li>
-                                <li><a href="#Activités">Activités</a></li>
-                                <li><a href="#Partenaires">Partenaires</a></li>
-                                <li><a href="#Réalisations">Réalisations</a></li>
-                                <li><a href="#Qui sommes-nous">Qui sommes-nous</a></li>
-                            </ul>
-                        </li>
-                                <li>Factory</li>
-                                <li>Studio</li>
-                                <li>Consulting</li>
-                                <li>Campus</li>
-                                <li id="nous">Qui sommes-nous</li>
+            <Header />        
 
-                    </ul>
-                </nav>
-            </header>
-
-            
-        
-
-            
-            <section id="a propos">
-                <h2> à propos</h2>
-                lorem ipsumWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.ai 
-            </section>
-
-           
+           <Bienvenue />
             <section id="Activités">
                 <h2>Activités</h2>
                 lorem ipsumWe love that you love Bito’s AI Code Completions!
---
+++ src/components/bienvenue.js
@@ -0,0 +1,20 @@
+import banner from "../assets/banner.png"
+import "../styles/bienvenue.css"
+
+export default function Bienvenue(){
+    return (
+        <>
+             <section id="bienvenue">
+                <div class="left">
+                    <h1>Bienvenue chez les génies de l'innovation!</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur. Cursus quis turpis eget dictum est. Phasellus consectetur suspendisse ac ut sed. Tempus non amet cras sed tempus luctus ut tempor. In tellus sit vel nunc amet.
+    Tempus non amet cras sed tempus luctus ut tempor. In tellus sit vel nunc amet</p>
+                    <button id="contactus">Contactez-nous</button>
+                </div>
+                <div class="right">
+                    <img src={banner} alt="banner" id="banner" />
+                </div>
+            </section>
+        </>
+    )
+}
