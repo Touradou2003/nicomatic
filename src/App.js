815e2671728097056b3eabbfa306060ae48cecd1
Touradou <cheikhtouradougueye@esp.sn>
2024-05-22 22:53:22 +0000

    logo partenaires

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Zone from './components/zone';
 import "./styles/body.css";
 import Cartes from './components/cartes';
 import Realisation from './components/realisation';
+import Partenaire from './components/partenaire';
 
 
 
@@ -22,26 +23,8 @@ function App() {
            <Zone />  
            <Cartes />
            <Realisation />
+           <Partenaire />
 
-
-            <section id="Qui sommes-nous">
-                <h2>Qui sommes-nous</h2>
-                lorem ipsumWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-You have hit your limit for AI Code Completions per day on your plan.
-To receive more Completions, please upgrade your plan or contact mailto:support@bito.ai 
-            </section>
         </div>
     );
 }
