feaeff7e7be6f31012a7bd5fcbbe6f06ff7d0d76
Touradou <cheikhtouradougueye@esp.sn>
2024-05-23 12:47:18 +0000

    premiere version

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,46 @@
-import footer from "../assets/footer.png"
-import '../styles/footer.css'
+import footer from "../assets/footer.png";
+import '../styles/footer.css';
 
-
-export default function Footer()
-{
-    return(
+export default function Footer() {
+    return (
         <>
             <section id="footer">
                 <div class="imgFooter">
                     <div>
-                    <img class="img" src={footer} alt="footer" />
-                        </div>
-                        <div>
+                        <img class="img" src={footer} alt="footer" />
+                    </div>
+                    <div class="formContainer">
+                        <form>
+                            <div class="bouge">
+                            <h2>Contactez-nous</h2>
+                            <label>
+                                Nom:
+                                <input type="text" name="name" />
+                            </label>
+                            <label>
+                                Prénom:
+                                <input type="text" name="firstname" />
+                            </label>
+                            <label>
+                                Téléphone:
+                                <input type="tel" name="phone" />
+                            </label>
+                            <label>
+                                Email:
+                                <input type="email" name="email" />
+                            </label>
+                            <label>
+                                Message:
+                                <textarea name="message"></textarea>
+                            </label>
+                            <input type="submit" value="Envoyer" />
 
-                        </div>
-                      
+                            </div>
+                           
+                        </form>
+                    </div>
                 </div>
-
             </section>
-     
-        
         </>
-    )
+    );
 }
